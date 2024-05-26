98529f69f55d7ddb57ec5932aea82d9e81c8c370
shosetim <shosetim32@gmail.com>
2024-05-26 13:36:22 +0800

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    {{ data }}
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      data: {},
+    };
+  },
+  mounted() {
+     console.log(import.meta.env.VITE_TEST);
+     const url = import.meta.env.VITE_PATH;
+    this.axios.get(url)
+      .then(res => {
+        console.log(res);
+        this.data = res.data.results[0];
+      })
+  },
+}
+</script>
